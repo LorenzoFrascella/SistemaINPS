0eb70cab07cf36e670b02b4b60573732f7620078
Lorenzo Frascella <lore.fras05@gmail.com>
2025-05-16 16:16:51 +0200

    commit

--- src/Dottore.java
+++ src/Dottore.java
@@ -19,6 +19,66 @@ public class Dottore {
         this.email = email;
     }
 
+    public void setIdDottore(String idDottore) {
+        this.idDottore = idDottore;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCognome() {
+        return cognome;
+    }
+
+    public void setCognome(String cognome) {
+        this.cognome = cognome;
+    }
+
+    public String getSpecializzazione() {
+        return specializzazione;
+    }
+
+    public void setSpecializzazione(String specializzazione) {
+        this.specializzazione = specializzazione;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public ArrayList<String> getOrariDisponibili() {
+        return orariDisponibili;
+    }
+
+    public void setOrariDisponibili(ArrayList<String> orariDisponibili) {
+        this.orariDisponibili = orariDisponibili;
+    }
+
+    public ArrayList<Paziente> getPazienti() {
+        return pazienti;
+    }
+
+    public void setPazienti(ArrayList<Paziente> pazienti) {
+        this.pazienti = pazienti;
+    }
+
     public void aggiungiPaziente(Paziente p) {
         pazienti.add(p);
     }
--- src/SistemaINPS.java
+++ src/SistemaINPS.java
@@ -1,3 +1,79 @@
+import java.util.ArrayList;
+
 public class SistemaINPS {
-    ArrayList <Paziente> pazienti = new ArrayList<>();
+    private ArrayList <Paziente> pazienti = new ArrayList<>();
+    private ArrayList <Dottore> dottori = new ArrayList<>();
+    private ArrayList <VisitaMedica> visite = new ArrayList<>();
+
+    public SistemaINPS(ArrayList<Paziente> pazienti, ArrayList<Dottore> dottori, ArrayList<VisitaMedica> visite) {
+        this.pazienti = pazienti;
+        this.dottori = dottori;
+        this.visite = visite;
+    }
+
+    public ArrayList<Paziente> getPazienti() {
+        return pazienti;
+    }
+
+    public void setPazienti(ArrayList<Paziente> pazienti) {
+        this.pazienti = pazienti;
+    }
+
+    public ArrayList<Dottore> getDottori() {
+        return dottori;
+    }
+
+    public void setDottori(ArrayList<Dottore> dottori) {
+        this.dottori = dottori;
+    }
+
+    public ArrayList<VisitaMedica> getVisite() {
+        return visite;
+    }
+
+    public void setVisite(ArrayList<VisitaMedica> visite) {
+        this.visite = visite;
+    }
+
+    public void aggiungiPaziente(Paziente p){
+        pazienti.add(p);
+    }
+
+    public void rimuoviPaziente(String codiceFiscale){
+        for (Paziente p : pazienti){
+            if(p.getCodiceFiscale().equalsIgnoreCase(codiceFiscale)){
+                pazienti.remove(p);
+            }
+        }
+    }
+
+    public void cercaPaziente(String codiceFiscale){
+        for (Paziente p : pazienti){
+            if(p.getCodiceFiscale().equalsIgnoreCase(codiceFiscale)){
+                System.out.println("paziente: " + p.getCognome());
+            }
+        }
+    }
+
+    public void aggiungiDottore(Dottore d){
+        dottori.add(d);
+    }
+
+    public void rimuoviDottore(String id){
+        for (Dottore d : dottori){
+            if(d.getIdDottore().equalsIgnoreCase(id)){
+                dottori.remove(d);
+            }
+        }
+    }
+
+    public void cercaDottore(String id){
+        for (Dottore d : dottori){
+            if(d.getIdDottore().equalsIgnoreCase(id)){
+                System.out.println("dottore: " + d.getCognome());
+            }
+        }
+    }
+
+
 }
